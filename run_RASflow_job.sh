4a4bf48188b99ae443868823b7d9242c80f008b4
neyugnhnil <neyugnhnil@proton.me>
2024-11-20 20:46:47 -0500

    first commit

--- run_RASflow_job.sh
+++ run_RASflow_job.sh
@@ -14,19 +14,11 @@
 # setup environment
 
 module load miniconda3/23.11.0
-module load R/3.5.1
-module load python/3.6.12
+
+source activate RASflow
 
 # for getting data
 module load sratoolkit/3.0.2
-
-# for rasflow
-module load salmon/1.0.0
-module load trimgalore/0.6.4
-module load fastqc/0.11.8
-module load samtools/1.9
-
-source activate RASflow
 
 pip install csvkit
 
